53985409732cec8858c6c6c0d0f4d234e837c8ee
rober <roberto-calo@hotmail.it>
2026-08-06 13:08:39 +0200

    cache

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,7 +5,6 @@ import { ErrorBoundary } from './components/ErrorBoundary'
 import './index.css'
 import App from './App.tsx'
 
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ErrorBoundary>
